92e6b6053e12cf564747171fa2c8bb462de77abf
nayVoo <124888494+nayVoo@users.noreply.github.com>
2026-03-12 22:38:13 -0400

    Update task08.rs

--- src/hackerrank/task08.rs
+++ src/hackerrank/task08.rs
@@ -1,40 +1,42 @@
-pub fn breaking_records(scores: &[i32]) -> (i32, i32) {
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+fn breaking_records(scores: &[i32]) -> [i32; 2] {
     let mut max_score = scores[0];
     let mut min_score = scores[0];
+    let mut max_count = 0;
+    let mut min_count = 0;
 
-    let mut max_breaks: i32 = 0;
-    let mut min_breaks: i32 = 0;
-
-    for score in scores {
-        if *score > max_score {
-            max_score = *score;
-            max_breaks += 1;
-        }
-
-        if *score < min_score {
-            min_score = *score;
-            min_breaks += 1;
+    for &score in scores.iter().skip(1) {
+        if score > max_score {
+            max_score = score;
+            max_count += 1;
+        } else if score < min_score {
+            min_score = score;
+            min_count += 1;
         }
     }
 
-    (max_breaks, min_breaks)
+    [max_count, min_count]
 }
 
-#[test]
-fn test0() {
-    let scores = vec![10, 5, 20, 20, 4, 5, 2, 25, 1];
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
 
-    let real = breaking_records(&scores);
-    let expected = (2, 4);
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
 
-    assert_eq!(real, expected);
+    let _n = stdin_iterator.next().unwrap().unwrap()
+        .trim().parse::<usize>().unwrap();
+
+    let scores: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect();
+
+    let result = breaking_records(&scores);
+
+    writeln!(&mut fptr, "{} {}", result[0], result[1]).ok();
 }
-
-#[test]
-fn test1() {
-    let scores = vec![3, 4, 21, 36, 10, 28, 35, 5, 24, 42];
-
-    let real = breaking_records(&scores);
-    let expected = (4, 0);
-
-    assert_eq!(real, expe }
