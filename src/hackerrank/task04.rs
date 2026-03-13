bd6cf279a510858276e4ae42f930d690d87ae632
nayVoo <124888494+nayVoo@users.noreply.github.com>
2026-03-12 22:52:48 -0400

    Update task04.rs

--- src/hackerrank/task04.rs
+++ src/hackerrank/task04.rs
@@ -38,3 +38,22 @@ fn main() {
         writeln!(fptr, "{}", g).ok();
     }
 }
+
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+fn grading_students(grades: &[i32]) -> Vec<i32> {
+    grades.iter().map(|&g| {
+        if g < 38 {
+            g
+        } else {
+            let next_multiple_of_5 = ((g / 5) + 1) * 5;
+            if next_multiple_of_5 - g < 3 {
+                next_multiple_of_5
+            } else {
+                g
+            }
+        }
+    }).collect()
+}
