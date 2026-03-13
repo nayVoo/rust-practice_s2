1c6e9ad837ca75c67d53bfb3b0bd4d39253d5982
nayVoo <124888494+nayVoo@users.noreply.github.com>
2026-03-12 22:20:25 -0400

    Add breaking_records function and unit tests
    Implement breaking_records function to track score breaks and add tests.

--- src/hackerrank/task08.rs
+++ src/hackerrank/task08.rs
@@ -1,1 +1,40 @@
-// https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+pub fn breaking_records(scores: &[i32]) -> (i32, i32) {
+    let mut max_score = scores[0];
+    let mut min_score = scores[0];
+
+    let mut max_breaks: i32 = 0;
+    let mut min_breaks: i32 = 0;
+
+    for score in scores {
+        if *score > max_score {
+            max_score = *score;
+            max_breaks += 1;
+        }
+
+        if *score < min_score {
+            min_score = *score;
+            min_breaks += 1;
+        }
+    }
+
+    (max_breaks, min_breaks)
+}
+
+#[test]
+fn test0() {
+    let scores = vec![10, 5, 20, 20, 4, 5, 2, 25, 1];
+
+    let real = breaking_records(&scores);
+    let expected = (2, 4);
+
+    assert_eq!(real, expected);
+}
+
+#[test]
+fn test1() {
+    let scores = vec![3, 4, 21, 36, 10, 28, 35, 5, 24, 42];
+
+    let real = breaking_records(&scores);
+    let expected = (4, 0);
+
+    assert_eq!(real, expe }
