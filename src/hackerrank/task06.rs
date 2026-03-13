ed8b34d688bb480b58d285369f717c5449f98351
nayVoo <124888494+nayVoo@users.noreply.github.com>
2026-03-12 21:53:20 -0400

    Update task06.rs

--- src/hackerrank/task06.rs
+++ src/hackerrank/task06.rs
@@ -1,1 +1,25 @@
-// https://www.hackerrank.com/challenges/kangaroo/problem
+pub fn kangaroo(x1: i32, v1: i32, x2: i32, v2: i32) -> String {
+    if v1 <= v2 {
+        return String::from("NO");
+    }
+
+    if (x2 - x1) % (v1 - v2) == 0 {
+        String::from("YES")
+    } else {
+        String::from("NO")
+    }
+}
+
+#[test]
+fn test0() {
+    let real = kangaroo(0, 3, 4, 2);
+    let expected = String::from("YES");
+    assert_eq!(real, expected);
+}
+
+#[test]
+fn test1() {
+    let real = kangaroo(0, 2, 5, 3);
+    let expected = String::from("NO");
+    assert_eq!(real, expected);
+}
