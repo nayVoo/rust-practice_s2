881417cac692a0295ff13c6057e2e10f25a6169c
nayVoo <124888494+nayVoo@users.noreply.github.com>
2026-03-12 21:53:36 -0400

    Add get_total_x function with test case
    Implement function to count integers between two sets.

--- src/hackerrank/task07.rs
+++ src/hackerrank/task07.rs
@@ -1,1 +1,43 @@
-// https://www.hackerrank.com/challenges/between-two-sets/problem
+pub fn get_total_x(a: &[i32], b: &[i32]) -> i32 {
+    let mut count: i32 = 0;
+
+    let mut candidate = 1;
+    while candidate <= 100 {
+        let mut valid = true;
+
+        for x in a {
+            if candidate % x != 0 {
+                valid = false;
+                break;
+            }
+        }
+
+        if valid {
+            for y in b {
+                if y % candidate != 0 {
+                    valid = false;
+                    break;
+                }
+            }
+        }
+
+        if valid {
+            count += 1;
+        }
+
+        candidate += 1;
+    }
+
+    count
+}
+
+#[test]
+fn test0() {
+    let a = vec![2, 4];
+    let b = vec![16, 32, 96];
+
+    let real = get_total_x(&a, &b);
+    let expected = 3;
+
+    assert_eq!(real, expected);
+}
