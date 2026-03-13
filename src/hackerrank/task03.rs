69f9bf256b4e8e37e6c3e0b4d5f2a6292bb022a5
nayVoo <124888494+nayVoo@users.noreply.github.com>
2026-03-12 21:51:53 -0400

    Add staircase function with test case
    Implement staircase function to print a staircase pattern.

--- src/hackerrank/task03.rs
+++ src/hackerrank/task03.rs
@@ -1,1 +1,17 @@
-// https://www.hackerrank.com/challenges/staircase/problem
+pub fn staircase(n: i32) {
+    for i in 1..=n {
+        let spaces = " ".repeat((n - i) as usize);
+        let hashes = "#".repeat(i as usize);
+        println!("{}{}", spaces, hashes);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_staircase_4() {
+        staircase(4);
+    }
+}
